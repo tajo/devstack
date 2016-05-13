ddaa27354cca98b1e55000d278f0a294dfcf2aa7
tajo <vojtech@miksu.cz>
2016-05-13 02:06:44 -0400

    nicer formatting of pure components

--- src/client/main.js
+++ src/client/main.js
@@ -3,8 +3,9 @@ import imgHello from './hello.gif';
 import imgIcon from './icon.png';
 import './main.less';
 
-export default () => <div className="hello">
-  Hello world!
-  <img src={imgHello} width="300px" alt="hello world" />
-  <img src={imgIcon} role="presentation" />
-</div>;
+export default () =>
+  <div className="hello">
+    Hello world!
+    <img src={imgHello} width="300px" alt="hello world" />
+    <img src={imgIcon} role="presentation" />
+  </div>;
--- src/server/html.js
+++ src/server/html.js
@@ -8,14 +8,15 @@ const appJS = isProduction ? buildDir.find(file => /^app\-\w+\.js$/.test(file)) 
 const appCSS = isProduction ? buildDir.find(file => /^app\-\w+\.css$/.test(file)) : '';
 const scripts = isProduction ? `/build/${appJS}` : '//localhost:8080/build/app.js';
 
-export default () => <html>
-  <head>
-    <meta charSet="utf-8" />
-    <title>First dev-stack</title>
-    {isProduction && <link href={`/build/${appCSS}`} rel="stylesheet" />}
-  </head>
-  <body>
-    <div id="app-root" />
-    <script src={scripts} type="text/javascript" />
-  </body>
-</html>;
+export default () =>
+  <html>
+    <head>
+      <meta charSet="utf-8" />
+      <title>First dev-stack</title>
+      {isProduction && <link href={`/build/${appCSS}`} rel="stylesheet" />}
+    </head>
+    <body>
+      <div id="app-root" />
+      <script src={scripts} type="text/javascript" />
+    </body>
+  </html>;
